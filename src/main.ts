215420f9ff7e8854abe340851d9611afd77c4d29
Dima Golubenko <dimagolubenko@gmail.com>
2022-08-22 22:03:45 +0300

    Add cookies decorator and edit app files

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   const PORT = process.env.APP_PORT;
 
   const config = new DocumentBuilder()
     .setTitle('NAVA ecommerce api')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
